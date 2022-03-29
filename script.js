d3ffdc03aa53d04e86230fa40ce035326a5e8ba0
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-29 17:46:52 +0200

    Conclusione parte js

--- script.js
+++ script.js
@@ -2,7 +2,9 @@
 // 2) Controlla che l'utente sia nella lista di chi può accedere
 // 3) Genera un messaggio a seconda che sia vero o falso
 
-const user = prompt("Inserisci la tua email");
+const user = prompt(
+  "Benvenuto utente, inserisci la lettera corrispondente alla tua Email. Email autorizzate finora: da A a F."
+);
 
 const authorizedMails = ["A", "B", "C", "D", "E", "F"];
 
@@ -20,7 +22,7 @@ while (!isInvited && i != authorizedMails.length) {
 }
 
 if (isInvited) {
-  document.writeln("Benvenuto!");
+  document.writeln("Benvenuto nel sistema!");
 } else {
   document.writeln("Accesso negato!");
 }
@@ -39,7 +41,11 @@ let scoreCPU = Math.floor(Math.random() * max) + 1;
 console.log(scoreCPU);
 
 if (scoreUser > scoreCPU) {
-  document.writeln("<br>Hai vinto con " + scoreUser);
+  document.writeln(
+    "<br>Hai vinto con " + scoreUser + " ! Il pc ha totalizzato: " + scoreCPU
+  );
 } else {
-  document.writeln("<br>Ha vinto il pc con " + scoreCPU);
+  document.writeln(
+    "<br>Ha vinto il pc con " + scoreCPU + " ! Tu hai totalizzato: " + scoreUser
+  );
 }
