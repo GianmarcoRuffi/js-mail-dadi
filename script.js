d366b9db3ed1a9a4ef7872cc90ef25b809a7c539
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-29 17:11:10 +0200

    Mail print

--- script.js
+++ script.js
@@ -2,15 +2,25 @@
 // 2) Controlla che l'utente sia nella lista di chi può accedere
 // 3) Genera un messaggio a seconda che sia vero o falso
 
-let user = prompt("Inserisci la tua email");
+const user = prompt("Inserisci la tua email");
 
 const authorizedMails = ["A", "B", "C", "D", "E", "F"];
 
-for (let i = 0; i < authorizedMails.length; i++) {
-  console.log(authorizedMails[i]);
+let isInvited = false;
+let i = 0;
+
+while (!isInvited && i != authorizedMails.length) {
+  console.log(i);
 
   if (user === authorizedMails[i]) {
-    console.log("Benvenuto " + user + " !");
-  } else user != authorizedMails[i];
-  console.log("Accesso Negato!");
+    isInvited = true;
+  }
+
+  i++;
+}
+
+if (isInvited) {
+  document.writeln("Benvenuto!");
+} else {
+  document.writeln("Accesso negato!");
 }
