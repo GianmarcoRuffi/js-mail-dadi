5bca4b50065531ad5c4d09c2a9bfd750144f0dca
GianmarcoRuffi <gianmarco.ruffi@outlook.it>
2022-03-30 00:37:48 +0200

    Creati index, css e js per ogni esercizio

--- script-mail.js
+++ script-mail.js
@@ -26,29 +26,3 @@ if (isInvited) {
 } else {
   document.writeln("Accesso negato!");
 }
-
-// 1) Definizione utenti
-// 2) Assegnazione dei dadi
-// 3) Generazione numero dei dadi
-// 4) Confronto tra i due risultati e assegnazione vincitore
-
-const max = 6;
-
-let scoreUser = Math.floor(Math.random() * max) + 1;
-console.log(scoreUser);
-
-let scoreCPU = Math.floor(Math.random() * max) + 1;
-console.log(scoreCPU);
-
-if (scoreUser > scoreCPU) {
-  document.writeln(
-    "<br>Hai vinto con: " + scoreUser + " ! Il pc ha totalizzato: " + scoreCPU
-  );
-} else {
-  document.writeln(
-    "<br>Ha vinto il pc con: " +
-      scoreCPU +
-      " ! Tu hai totalizzato: " +
-      scoreUser
-  );
-}
